cd55d99ddf0576a8db3c2064abc19f0462ea608d
yeison ospino <jeison1500@hotmail.com>
2023-06-27 00:40:47 -0500

    portafolio v.1

---
+++ js/script.js
@@ -0,0 +1,86 @@
+// animaciones 
+var typed = new Typed('.typing ',{
+    strings: ["Desarrollador Web", "Desarrollador de Software", "Desarrollador de Aplicaciones Moviles"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true    
+});
+
+// aside navbar
+const nav = document.querySelector(".nav"),
+        navList = nav.querySelectorAll("li"),
+        totalNavList = navList.length,
+        allSection = document.querySelectorAll(".section"),
+        totalSection = allSection.length;
+        for(let i=0; i<totalNavList; i++)
+        {
+             const a = navList[i].querySelector("a");
+            a.addEventListener("click", function()
+            {
+                for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.remove("back-section");
+            }
+                for(let j=0; j<totalNavList; j++)
+                {
+                    if(navList[j].querySelector("a").classList.contains("active"))
+                    {
+                        allSection[j].classList.add("back-section");
+                    }
+                    navList[j].querySelector("a").classList.remove("active");
+                }
+                this.classList.add("active")
+                showSection(this);
+                if(window.innerWidth < 1200)
+                {
+                    asideSectionTogglerBtn();
+                }
+            }) 
+        }
+        function showSection(element)
+        {
+            for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.remove("active");
+            }
+           const target  = element.getAttribute("href").split("#")[1];             
+            document.querySelector("#" + target).classList.add("active");
+        }
+        function updateNav(element)
+        {
+            for (let i = 0; i < totalNavList; i++)
+             {
+                navList[i].querySelector("a").classList.remove("active");
+                const target  = element.getAttribute("href").split("#")[1];
+                if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+                {
+                    navList[i].querySelector("a").classList.add("active");
+                }
+            }
+        }
+        document.querySelector(".hire-me").addEventListener("click", function()
+        {
+            showSection(this);
+            updateNav(this);
+        })
+
+        const navTogglerBtn = document.querySelector(".nav-toggler"),
+        aside = document.querySelector(".aside");
+        navTogglerBtn.addEventListener("click", () =>
+        {
+            asideSectionTogglerBtn();
+        })
+        function asideSectionTogglerBtn()
+        {
+            aside.classList.toggle("open");
+            navTogglerBtn.classList.toggle("open");
+            for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.toggle("open");
+            }
+        }
+
+        // formulario de contacto
+       
+
+            
